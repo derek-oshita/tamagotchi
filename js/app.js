40af6a417ad767696989985d435eb160c43eb7ab
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-06-22 16:19:32 -0700

    Object properties properly reflected in DOM elements

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 class Tamagotchi {
     constructor (tamagotchiName) {
     // properties 
-        this.name = tamagotchiName || 'Charmander';
+        this.name = tamagotchiName;
         this.age = 0; 
         this.boredom = 0; 
         this.hunger = 0; 
@@ -31,19 +31,38 @@ class Tamagotchi {
 // --- Global Variables --- // 
 
 let time = 0; 
+let round = 1; 
+let tamagotchi; 
+let tamagotchiName; 
+let tamagotchiAge; 
+let tamagotchiBoredom; 
+let tamagotchiHunger; 
+let tamagotchiSleepiness; 
 
 // --- Functions --- // 
 
-// Create tamagotchi
+// Create tamagotchi - global variables don't get assigned until the user creates a tamagotchi 
+
 function createTamagotchi () {
-    let name = $('#input-name').val(); 
-    let newTamagotchi = new Tamagotchi(name);
-    $(nameField).html(name); 
-    $(ageField).html(0);   
-    $(stageField).html(1); 
-    // startTimer (); 
-    console.log(newTamagotchi); 
+    let nameInput = $('#input-name').val(); 
+    // assigning of values from new object to global variables
+    tamagotchi = new Tamagotchi (nameInput); 
+    tamagotchi.name = nameInput || 'Charmander'; 
+    tamagotchiName = nameInput || 'Charmander'; 
+    tamagotchiAge = Tamagotchi.age; 
+    tamagotchiBoredom = Tamagotchi.boredom; 
+    tamagotchiHunger = Tamagotchi.hunger; 
+    tamagotchiSleepiness = Tamagotchi.sleepiness; 
+    // inserting those values to the DOM using jQuery 
+    $(nameField).html(tamagotchi.name);
+    $(ageField).html(tamagotchi.age);
+    $(boredomField).html(tamagotchi.boredom); 
+    $(hungerField).html(tamagotchi.hunger); 
+    $(sleepinessField).html(tamagotchi.sleepiness);   
+    console.log(tamagotchi); 
 }; 
+
+
 
 // Start Timer 
 function startTimer () {
@@ -59,16 +78,20 @@ function startTimer () {
 
 // Cached DOM Elements 
 
-// Create tamagotchi
-let name = $('#input-name').val(); 
+// Buttons 
 const createButton = $(`#create`); 
 
-// Age and name
-let nameField = $('#name'); 
-let ageField = $('#age'); 
+// Tamagotchi Details
+const nameField = $('#name'); 
+const ageField = $('#age'); 
+
+// Tamagotchi Metrics
+const boredomField = $('#boredom'); 
+const hungerField = $('#hunger'); 
+const sleepinessField = $('#sleepiness');  
 
 // Stage
-let stageField = $('#stage'); 
+const stageField = $('#stage'); 
 
 
 // Event listeners
